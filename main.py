34e8fca4e3cc96dbd2d890b8c7511d1c75bf1a34
sour cream <jeroriver2004@gmail.com>
2023-03-17 08:20:27 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+class Blackjack:
+   def __init__(self):
+       self.player_name = None
+       self.player_chips = 100
+
+
+   def register_player(self):
+       """
+       Registra el nombre del jugador
+       """
+       pass
+
+
+   def start_game(self):
+       """
+       Inicia el juego y reparte las cartas
+       """
+       pass
+
+
+   def player_move(self):
+       """
+       Realiza la jugada del jugador
+       """
+       pass
+
+
+   def house_move(self):
+       """
+       Realiza la jugada de la casa
+       """
+       pass
+
+
+   def end_game(self):
+       """
+       Finaliza el juego y muestra los resultados
+       """
+       pass
+
